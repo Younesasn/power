d76049c53feff63bde9e8535ca9f875f51389b64
ASSANI Younes <younesassani@gmail.com>
2024-04-02 11:13:40 +0200

    add config.ini

--- data/cnx.php
+++ data/cnx.php
@@ -1,12 +1,16 @@
 <?php
-$db_host = 'localhost';
-$db_user = 'youpower';
-$db_password = '6ym[i9)m5(2Fh]3M';
-$db_db = 'power';
+[
+    'DB_HOST' => $db_host,
+    'DB_NAME' => $db_name,
+    'DB_USERNAME' => $db_user,
+    'DB_PASSWORD' => $db_password,
+    'DB_CHARSET' => $db_charset
+    
+] = parse_ini_file('config/db.ini');
 
 try {
     // On se connecte à MySQL
-    $bdd = new PDO('mysql:host='.$db_host.';dbname='.$db_db.';charset=utf8', $db_user, $db_password, [
+    $bdd = new PDO("mysql:host=$db_host;dbname=$db_name;charset=$db_charset", $db_user, $db_password, [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
